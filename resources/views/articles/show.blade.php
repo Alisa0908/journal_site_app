3d3c030bd42522f288bf68cd77a871bc6cc9c89b
Alisa0908 <alisa0908piano@gmail.com>
2021-09-21 23:23:54 +0900

    課題1完了

---
+++ resources/views/articles/show.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+    <h1>
+        <b>論文詳細</b>
+    </h1>
+    <p>タイトル : {{ $article->title }}</p>
+    <p>{{ $article->body }}</p>
+
+    <button type="button" onclick="location.href='/articles'">一覧へ戻る</button>
+    <button type="button" onclick="location.href='/articles/{{ $article->id }}/edit'">編集する</button>
+    <input type="submit" value="削除する" onclick="if(!confirm('本当に削除しますか?')){return false}" form="delete">    
+    <form action="articles/{{ $article->id }}" method="post" id="delete">
+        @csrf
+        @method('DELETE')
+    </form>
+</body>
+</html>
